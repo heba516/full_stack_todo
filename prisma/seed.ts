e0dd613ee416e5ee6b862a6410a8b64f369a7684
Heba Yasser <hebay99@gmail.com>
2024-10-18 02:20:03 +0300

    add todo model

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -4,20 +4,30 @@ import { faker } from "@faker-js/faker"
 const prisma = new PrismaClient()
 
 async function main() {
-    await prisma.user.createMany({
+
+  await prisma.todo.createMany({
       data: Array.from({ length : 20 }, () => (
           {
-          email: faker.internet.email(),
-          name: faker.internet.userName(),
-          address: {
-            street: faker.location.street(),
-            city: faker.location.city(),
-            state: faker.location.state(),
-            zip: faker.location.zipCode(),
-          }
+          title: faker.lorem.text(),
+          body: faker.lorem.paragraph()
       }
         ))
   })
+
+  //   await prisma.user.createMany({
+  //     data: Array.from({ length : 20 }, () => (
+  //         {
+  //         email: faker.internet.email(),
+  //         name: faker.internet.userName(),
+  //         address: {
+  //           street: faker.location.street(),
+  //           city: faker.location.city(),
+  //           state: faker.location.state(),
+  //           zip: faker.location.zipCode(),
+  //         }
+  //     }
+  //       ))
+  // })
 }
 
 main()
